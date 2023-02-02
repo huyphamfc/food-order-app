c3135942b6da9db27d7468ff75c141da1d70b189
huyphamfc <huyphamfc@gmail.com>
2023-02-02 16:21:26 +0700

    create GlobalStyles component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { GlobalStyles } from 'components';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement);
-root.render(<App />);
+root.render(
+  <GlobalStyles>
+    <App />
+  </GlobalStyles>,
+);
