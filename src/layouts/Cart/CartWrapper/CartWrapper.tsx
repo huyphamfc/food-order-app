96c3ba459a9d24c7c4ad8c39b0c7b4d685915d01
huyphamfc <huyphamfc@gmail.com>
2023-02-05 08:18:39 +0700

    calculate Total Price

--- src/layouts/Cart/CartWrapper/CartWrapper.tsx
+++ src/layouts/Cart/CartWrapper/CartWrapper.tsx
@@ -1,39 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from './CartWrapper.module.scss';
 import CartItem from '../CartItem';
 import { Button } from 'components';
+import { RootState } from 'store';
 
 type CartProps = {
   onHandleToggleModal(): void;
 };
 
 function CartWrapper({ onHandleToggleModal }: CartProps) {
-  const dummyData = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      price: 22.99,
-      amount: '1',
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      price: 16.5,
-      amount: '1',
-    },
-  ];
+  const { meals } = useSelector((state: RootState) => state.cart);
 
-  const meals = dummyData.map(({ id, ...meal }) => <CartItem key={id} {...meal} />);
+  const mealsUI = meals.map((meal) => <CartItem key={meal.id} {...meal} />);
+
+  const total = meals
+    .reduce((prevTotal, { price, amount }) => prevTotal + price * amount, 0)
+    .toFixed(2);
 
   return (
     <>
       <div className={styles.cart__overlay} onClick={onHandleToggleModal} />
       <div className={styles.cart__content}>
-        <ul>{meals}</ul>
+        <ul>{mealsUI}</ul>
         <div className={styles.cart__total}>
           <span>Total</span>
-          <span>$999</span>
+          <span>${total}</span>
         </div>
         <div className={styles.cart__actions}>
           <Button solid onClick={onHandleToggleModal}>
